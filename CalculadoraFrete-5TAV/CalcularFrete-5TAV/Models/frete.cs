0b2e9f16444eddceae65b8db67d8214fdd57668d
JuanLFS <JUANLUIS-FONSECA@HOTMAIL.COM>
2022-04-05 22:45:00 +0100

    Calculadora frete

---
+++ CalculadoraFrete-5TAV/CalcularFrete-5TAV/Models/frete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalcularFrete_5TAV.Models
+{
+    public class Frete
+    {
+        public int Cep { get; set; }
+        public double Peso { get; set; }
+
+        private ICorreioService correioService;
+
+        public Frete(ICorreioService _correioService)
+        {
+            correioService = _correioService;
+        }
+
+        public double CalcularFrete()
+        {
+            return correioService.CalcularFrete();
+        }
+    }
+}
