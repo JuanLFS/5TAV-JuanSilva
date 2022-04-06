2601f36bf9270f22121303276bb0e3539e1a723d
JuanLFS <JUANLUIS-FONSECA@HOTMAIL.COM>
2022-04-06 16:20:00 +0100

    Frete App 2

---
+++ FreteApp02/FreteApp02/Frete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreteApp02
+{
+    public class Frete
+    {
+        public int Cep { get; set; }
+        public double Peso { get; set; }
+
+        private ICorreioService correioService;
+
+        public Frete(ICorreioService _correioService)
+        {
+            correioService = _correioService;
+        }
+
+        public double CalcularFrete()
+        {
+            return correioService.CalculaFrete();
+        }
+    }
+}
