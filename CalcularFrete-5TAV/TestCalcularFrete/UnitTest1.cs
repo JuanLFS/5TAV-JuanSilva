6854e575646ab3347423c146a21f0171264affa0
JuanLFS <79913501+JuanLFS@users.noreply.github.com>
2022-04-21 18:58:06 -0300

    CalcularFrete-5TAV

--- CalcularFrete-5TAV/TestCalcularFrete/UnitTest1.cs
+++ CalcularFrete-5TAV/TestCalcularFrete/UnitTest1.cs
@@ -1,3 +1,5 @@
+using CalcularFrete_5TAV.Models;
+using Moq;
 using System;
 using Xunit;
 
@@ -6,9 +8,21 @@ namespace TestCalcularFrete
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestFrete()
         {
 
+            //Arrange
+            Mock<ICorreioService> mock = new Mock<ICorreioService>();
+            mock.Setup(m => m.CalcularFrete()).Returns(7.52);
+            Frete frete = new Frete(mock.Object)
+            {
+                Cep = 21857818,
+                Peso = 5
+            };
+            //Act
+            double resultado = frete.CalcularFrete();
+            //Assert
+            Assert.Equal(7.52, resultado);
         }
     }
 }
